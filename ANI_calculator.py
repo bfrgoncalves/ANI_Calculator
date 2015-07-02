8222b2ffbf65cfa6a16e0c13dbfa38c1a13e64f1
bfrgoncalves <brunofiliperg@gmail.com>
2015-07-02 16:17:33 +0100

    version2

--- ANI_calculator.py
+++ ANI_calculator.py
@@ -9,17 +9,13 @@ from os.path import isfile, join
 def main():
 
 	parser = argparse.ArgumentParser(description="This program performs the ANI method (ANIb or ANIm)")
-	parser.add_argument('-i', nargs='?', type=str, help="folder with fna files", required=True)
+	parser.add_argument('-i', nargs='?', type=str, help="folder with fna files", required=False)
+	parser.add_argument('-d', nargs='?', type=str, help="Token to download from NCBI/Bacteria", required=False)
 	parser.add_argument('-o', nargs='?', type=str, help='Destination folder', required=True)
 	parser.add_argument('-c', nargs='?', type=bool, help="is cluster version", required=True)
 	parser.add_argument('-t', nargs='?', type=str, help="type of ANI (ANIm or ANIb)", required=True)
 
 	args = parser.parse_args()
-
-	fnaGenomes = args.i
-	ANIout = args.o
-	isCluster = args.c
-	typeANI = args.t
 
 	if args.c:
 		nonCluster(args)
@@ -29,7 +25,15 @@ def main():
 
 def nonCluster(args):
 	print 'Running'
-	subprocess.call(['python', 'pyani_version2/average_nucleotide_identity.py','-i', args.i, '-o',  args.o,  '-m', args.t]);
+	if args.d:
+		subprocess.call(['python', 'dwnFTP/dwnFTP.py', args.d, 'InputFiles', '*.fna']);
+		subprocess.call(['python', 'pyani_version2/average_nucleotide_identity.py','-i', 'InputFiles/' + args.d, '-o',  args.o,  '-m', args.t]);
+	else:
+		if not args.i:
+			print 'Please select the folder with query files'
+			os.exit()
+		
+		subprocess.call(['python', 'pyani_version2/average_nucleotide_identity.py','-i', args.i, '-o',  args.o,  '-m', args.t]);
 
 
 def cluster(args):
