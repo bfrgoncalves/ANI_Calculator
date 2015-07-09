4f40894d09ae236fb587c2452d43a5a16faf5ae2
bfrgoncalves <brunofiliperg@gmail.com>
2015-07-09 11:39:11 +0100

    clustering

--- ANI_calculator.py
+++ ANI_calculator.py
@@ -156,7 +156,7 @@ def cluster(args):
 	os.chdir(str(homeFolder))
 	os.system("rm *py.o*")
 
-	subprocess.call(['python', 'hierarchical_clustering.py', '--i', os.path.join(str(args.o), resultFileName])
+	subprocess.call(['python', 'hierarchical_clustering.py', '--i', os.path.join(str(args.o), resultFileName)])
 
 	print 'Download Time: ' + str(timeDownload)
 	print 'ANI Time: ' + str(timeANI)
